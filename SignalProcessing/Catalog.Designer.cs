5adcdce0c6bdba28ad0023d7979c185e95882d7a
Nikonoffcmi <114634910+Nikonoffcmi@users.noreply.github.com>
2022-10-26 13:00:39 +0700

    Работающий вариант 1

--- SignalProcessing/Catalog.Designer.cs
+++ SignalProcessing/Catalog.Designer.cs
@@ -31,6 +31,7 @@
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.button3 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -41,7 +42,9 @@
             this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Control;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Cursor = System.Windows.Forms.Cursors.Hand;
             this.dataGridView1.Location = new System.Drawing.Point(13, 13);
             this.dataGridView1.MultiSelect = false;
             this.dataGridView1.Name = "dataGridView1";
@@ -52,8 +55,10 @@
             // 
             // button3
             // 
-            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.button3.Location = new System.Drawing.Point(696, 407);
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.Location = new System.Drawing.Point(159, 407);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(92, 31);
             this.button3.TabIndex = 3;
@@ -63,6 +68,7 @@
             // 
             // button1
             // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button1.Location = new System.Drawing.Point(12, 407);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(100, 31);
@@ -71,11 +77,24 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.button2.Location = new System.Drawing.Point(696, 407);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(92, 31);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Сохранить";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Catalog
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.dataGridView1);
@@ -91,5 +110,6 @@
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
--- SignalProcessing/SignalProcessingMain.cs
+++ SignalProcessing/SignalProcessingMain.cs
@@ -3,12 +3,16 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Security;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Xml.Linq;
+using static System.Net.WebRequestMethods;
 
 namespace SignalProcessing
 {
@@ -17,24 +21,75 @@ namespace SignalProcessing
 
         HandlerComposite handlers;
 
+        string filePath;
+
         public SignalProcessingMain()
         {
             InitializeComponent();
             handlers = new HandlerComposite();
+            label3.Text = "";
         }
 
         private void HandlerOneToolStripMenuItem_Click(object sender, EventArgs e)
         {
             var CatalogHandlerOne = new Catalog<List<HandlerOne>, HandlerOne>(handlers.GethandlerOne(), handlers);
-            CatalogHandlerOne.Show();
-            this.handlers = CatalogHandlerOne.handlers;
+            if (CatalogHandlerOne.ShowDialog() == DialogResult.OK)
+                UpdateList();
         }
 
         private void HandlerTwoToolStripMenuItem_Click(object sender, EventArgs e)
         {
             var CatalogHandlerTwo = new Catalog<List<HandlerTwo>, HandlerTwo>(handlers.GethandlerTwo(), handlers);
-            CatalogHandlerTwo.Show();
-            this.handlers = CatalogHandlerTwo.handlers;
+            if (CatalogHandlerTwo.ShowDialog() == DialogResult.OK)
+                UpdateList();
+        }
+
+        private void UpdateList()
+        {
+            listBox1.Items.Clear();
+            var i = 1;
+            foreach (var name in handlers.GetNames())
+                listBox1.Items.Add(i++.ToString() + ". " + name);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var file = new OpenFileDialog()
+            {
+                FileName = "Select a text file",
+                Filter = "Text files (*.txt)|*.txt",
+                Title = "Open text file"
+            };
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    var sr = new StreamReader(file.FileName);
+                    textBox1.Text = (sr.ReadToEnd());
+                    filePath = file.FileName;
+                    sr.Close();
+                }
+                catch (SecurityException ex)
+                {
+                    MessageBox.Show($"Security error.\n\nError message: {ex.Message}\n\n" +
+                    $"Details:\n\n{ex.StackTrace}");
+                }
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var numbers = textBox1.Text.Split(new char[] { ' ' }).Select(n => double.Parse(n)).ToList<double>();
+            handlers.RunProcessing(numbers);
+
+            label3.Text = "";
+            foreach (var number in numbers)
+                label3.Text += number.ToString() + " ";
+
+            var sw = new StreamWriter(filePath);
+            sw.WriteLine(label3.Text.Remove(label3.Text.Length - 1));
+            sw.Close();
         }
     }
 }
